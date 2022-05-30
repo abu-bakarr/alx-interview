059c1acf04f3644fb3073c7a7f7fe639c25fa878
abu-bakarr <abangura14@yahoo.com>
2022-05-30 14:07:15 +0000

    Add file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """ Prototype: def makeChange(coins, total)
+        Return: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewestNumber = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buffer = total // coin
+        fewestNumber += buffer
+        total -= (buffer * coin)
+    if total != 0:
+        return -1
+    return fewestNumber
